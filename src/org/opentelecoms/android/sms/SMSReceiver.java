6c2e55d5e7c98128690aeb940477199f90760f50
Daniel Pocock <daniel@pocock.com.au>
2011-06-14 21:30:46 +0200

    Save local copy of registration data.  Populate preferences for SIP when auth code processed.

--- src/org/opentelecoms/android/sms/SMSReceiver.java
+++ src/org/opentelecoms/android/sms/SMSReceiver.java
@@ -4,6 +4,7 @@ import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.opentelecoms.android.reg.RegistrationPhaseTwo;
 import org.opentelecoms.android.sip.RegistrationFailedException;
 import org.opentelecoms.android.sip.RegistrationUtil;
 import org.sipdroid.sipua.R;
@@ -26,73 +27,8 @@ public class SMSReceiver extends BroadcastReceiver {
 	static final String ACTION =
 		"android.provider.Telephony.SMS_RECEIVED";
 
-	
-	
 	static Pattern pattern = Pattern.compile("(\\*\\*\\p{Alnum}{6}\\*\\*)");
 
-	protected String getBodyXml(String regCode) {
-
-		XmlSerializer serializer = Xml.newSerializer();
-		StringWriter writer = new StringWriter();
-		try {
-			serializer.setOutput(writer);
-			serializer.startDocument("UTF-8", true);
-			serializer.startTag("", "activation");
-		
-			RegistrationUtil.serializeProperty(serializer, "regCode", regCode);
-			
-			serializer.endTag("", "activation");
-			serializer.endDocument();
-			return writer.toString();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		} 
-	}
-	
-	protected String getEncryptedXml(Context context, String s) {
-		XmlSerializer serializer = Xml.newSerializer();
-		StringWriter writer = new StringWriter();
-		try {
-			serializer.setOutput(writer);
-			serializer.startDocument("UTF-8", true);
-			serializer.startTag("", "encryptedActivation");
-			//serializer.attribute("", "regNum", getRegNum());
-
-			serializer.text(RegistrationUtil.getEncryptedStringAsBase64(context, s));
-
-			serializer.endTag("", "encryptedActivation");
-			serializer.endDocument();
-			return writer.toString();
-		} catch (Exception e) {
-			Log.e(LOG_TAG, e.toString());
-			return null;
-		}
-	}	
-	
-	
-	
-	protected void handleRegistrationCode(final Context context, final String regCode) {
-		(new Thread() {
-			public void run() {
-				
-				try {
-					
-				
-					String s = getBodyXml(regCode);
-					RegistrationUtil.submitMessage(getEncryptedXml(context, s));  
-
-				    
-				
-				} catch (RegistrationFailedException e) {
-					// TODO: display error to user
-					Log.e(LOG_TAG, e.toString());
-				}
-				//mHandler.sendEmptyMessage(0);
-			}
-		}).start();
-	}
-	
-	
 	public void onReceive(Context context, Intent intent) {
 		try {
 		if (intent.getAction().equals(ACTION)) {
@@ -139,7 +75,14 @@ public class SMSReceiver extends BroadcastReceiver {
 		}
 	}
 	
-
+	protected void handleRegistrationCode(Context context, String regCode) {
+		// Send a new intent
+		
+		final Intent intent = new Intent(RegistrationPhaseTwo.ACTION);
+		//intent.setAction(RegistrationPhaseTwo.ACTION);
+		intent.putExtra(RegistrationPhaseTwo.REG_CODE, regCode);
+		context.sendBroadcast(intent);
+	}
 
 
 }
