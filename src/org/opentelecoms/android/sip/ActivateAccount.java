a6a66fad384eff029fdee83b5bc9a52f3cf37567
Daniel Pocock <daniel@pocock.com.au>
2011-06-24 19:38:06 +0200

    BUG26: button for returning to registration form

--- src/org/opentelecoms/android/sip/ActivateAccount.java
+++ src/org/opentelecoms/android/sip/ActivateAccount.java
@@ -6,6 +6,7 @@ import org.sipdroid.sipua.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +15,14 @@ import android.widget.Toast;
 public class ActivateAccount extends Activity {
 	
 	private final static int REG_CODE_SIZE = 10;
+	private final static String DEFAULT_REG_CODE = "**";
 	
 	private EditText etCode;
 	private Button buttonOK;
+
+	private Button buttonRegAgain;
+	
+	private static final String TAG = "ActAcct";
 	
 	protected void activateAccountNow() {
 		String regCode = etCode.getText().toString();
@@ -54,7 +60,19 @@ public class ActivateAccount extends Activity {
 				activateAccountNow();
 			}
 		});
+		
+		buttonRegAgain = (Button) findViewById(R.id.Button02);
+		buttonRegAgain.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				final Intent intent = new Intent(ActivateAccount.this, RegisterAccount.class);
+		        Log.v(TAG, "registration sent, going to activation window");
+		        startActivity(intent);
+		        finish();
+			}
+		});
+		
         etCode = (EditText) findViewById(R.id.EditText01);
+        etCode.setText(DEFAULT_REG_CODE);
         
         // TODO: we should check for any SMS in the inbox,
         // and activate automatically if one is found
