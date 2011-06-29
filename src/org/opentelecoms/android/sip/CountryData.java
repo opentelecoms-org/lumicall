2ce0d3bcec040846a815f2fed1c54500917dfb13
Daniel Pocock <daniel@pocock.com.au>
2011-06-29 22:21:28 +0200

    BUG3: Recognise all ISO 2 letter country codes when populating form

---
+++ src/org/opentelecoms/android/sip/CountryData.java
@@ -0,0 +1,39 @@
+package org.opentelecoms.android.sip;
+
+public class CountryData {
+	
+	private String name;
+	private String isoCountryCode;
+	private String ituCountryCode;
+	
+	public CountryData(String _name, String _isoCountryCode, String _ituCountryCode) {
+		setName(_name);
+		setIsoCountryCode(_isoCountryCode);
+		setItuCountryCode(_ituCountryCode);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getIsoCountryCode() {
+		return isoCountryCode;
+	}
+	
+	public void setIsoCountryCode(String isoCountryCode) {
+		this.isoCountryCode = isoCountryCode;
+	}
+	
+	public String getItuCountryCode() {
+		return ituCountryCode;
+	}
+	
+	public void setItuCountryCode(String ituCountryCode) {
+		this.ituCountryCode = ituCountryCode;
+	}
+
+}
