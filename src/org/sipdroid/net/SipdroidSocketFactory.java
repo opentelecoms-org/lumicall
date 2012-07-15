dc33f080eaa3a0f372bad4a53065d2fb7510e59f
Daniel Pocock <daniel@pocock.com.au>
2012-07-15 02:24:22 +0200

    Update for use with unpatched ice4j r314

--- src/org/sipdroid/net/SipdroidSocketFactory.java
+++ src/org/sipdroid/net/SipdroidSocketFactory.java
@@ -11,24 +11,8 @@ import org.ice4j.socket.DatagramSocketFactory;
 public class SipdroidSocketFactory implements DatagramSocketFactory {
 
 	@Override
-	public DatagramSocket createDatagramSocket() throws SocketException {
+	public DatagramSocket createUnboundDatagramSocket() throws SocketException {
 		return new SipdroidSocket();
-	}
-
-	@Override
-	public DatagramSocket createDatagramSocket(int port) throws SocketException, UnknownHostException {
-		return new SipdroidSocket(port);
-	}
-
-	@Override
-	public DatagramSocket createDatagramSocket(int port, InetAddress laddr) throws SocketException {
-		return new SipdroidSocket(port, laddr);
-	}
-
-	@Override
-	public DatagramSocket createDatagramSocket(SocketAddress bindaddr) throws SocketException {
-		// TODO Auto-generated method stub
-		return new SipdroidSocket(bindaddr);
 	}
 
 }
