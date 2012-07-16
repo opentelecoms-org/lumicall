0baaf7259c99948dbf5aa104ffc0333943001dc2
Daniel Pocock <daniel@pocock.com.au>
2012-07-16 22:14:38 +0200

    Add hashCode() and equals() to DBObject

--- src/org/lumicall/android/db/DBObject.java
+++ src/org/lumicall/android/db/DBObject.java
@@ -59,4 +59,26 @@ public abstract class DBObject {
 	
 	public abstract String getKeyForIntent();
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DBObject other = (DBObject) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 }
